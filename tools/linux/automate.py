947d93f98b201b972587ea5945c00a50bef735ed
scrdt <socuraidat@gmail.com>
2023-07-29 19:47:14 +0000

    1

--- tools/linux/automate.py
+++ tools/linux/automate.py
@@ -21,9 +21,12 @@ def install_qt():
   # qt
   if not base.is_file("./qt_source_5.9.9.tar.xz"):
     base.download("https://download.qt.io/new_archive/qt/5.9/5.9.9/single/qt-everywhere-opensource-src-5.9.9.tar.xz", "./qt_source_5.9.9.tar.xz")
+    print("##### NOT FILE")
+    print(os.getcwd())
 
   if not base.is_dir("./qt-everywhere-opensource-src-5.9.9"):
     base.cmd("tar", ["-xf", "./qt_source_5.9.9.tar.xz"])
+    print("#### CANT UNZIP")
 
   qt_params = ["-opensource",
                "-confirm-license",
